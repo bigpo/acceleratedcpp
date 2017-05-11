c1356da65a5346a4f1f523f331c0d0b0f6d6a377
bigpo <space1201@gmail.com>
2017-05-11 09:36:10 +0800

    add a main file

---
+++ ch2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "Please enter your name " << endl;
+    string name;
+    cin >> name;
+   
+    return 0;
+}
