691bdedb58a3ca4874d7ea95421f07ce1ace35e4
bigpo <space1201@gmail.com>
2017-05-15 12:08:14 +0800

    add some file too

--- ch2/main.cpp
+++ ch2/main.cpp
@@ -6,8 +6,30 @@ using namespace std;
 int main()
 {
     cout << "Please enter your name " << endl;
-    string name;
-    cin >> name;
-   
+    string name1;
+    string name2;
+    string name3;
+    cin >> name1 >> name2 >> name3;;
+    cout << "hello, " + name1 << endl;
+    cout << "Hello, " << name2 << endl;
+    cout << "Hello, " << name3 << endl;
+
+    
+    const string hello = "Hello";
+    const string message = hello + "world" + "!";
+    
+    const string exclam = "!";
+    const string message2 =  exclam + "Hello" + ", world!";
+
+    {
+        const string s = "a string";
+        cout << s << endl;
+        {
+            const string s = "another string";
+            cout << s << endl;
+        };
+    }
+
+
     return 0;
 }
